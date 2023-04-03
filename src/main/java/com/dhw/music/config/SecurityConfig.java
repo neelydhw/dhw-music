cd3e326aed778e900033717844d2ef8feae3c32e
dhw <neelydhw@hotmail.com>
2023-04-03 12:20:57 +0800

    用户接口BUG

--- src/main/java/com/dhw/music/config/SecurityConfig.java
+++ src/main/java/com/dhw/music/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.dhw.music.filter.JwtAuthorizationFilter;
 import com.dhw.music.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -35,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-//                .antMatchers(HttpMethod.POST,SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager()))
